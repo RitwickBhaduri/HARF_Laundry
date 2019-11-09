4cfef5e146a96439d2c7e53e87fd29594aedb9b8
RitwickBhaduri <54306152+RitwickBhaduri@users.noreply.github.com>
2019-11-09 16:33:27 +0530

    Add files via upload

---
+++ dbcon.php
@@ -0,0 +1,12 @@
+<?php
+	$con="harf_laundry";
+	$con=new mysqli ('localhost','root','',$con) or die("Unable to connect");
+	/*if($con)
+	{
+		echo "Success!!";
+	}
+	else
+	{
+		echo "Fail!!";
+	}*/
+?>
